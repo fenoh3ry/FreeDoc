1e0c8749ced972005e15c4833bd4167d7e67ebf1
fenoh3ry <fenohery.fanomezanirina@gmail.com>
2019-08-01 16:34:48 +0300

    test1

---
+++ db/migrate/20190801130908_patients.rb
@@ -0,0 +1,9 @@
+class Patients < ActiveRecord::Migration[6.0]
+  def change
+    create_table :patients do |t|
+      t.string :first_name
+      t.string :last_name
+      t.timestamps
+    end
+  end
+end
